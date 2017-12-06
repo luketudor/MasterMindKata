c0f2b825416ef2ad05abc87911a7690e0b01e062
luketudor <Luke.Tudor@myob.com>
2017-12-07 11:24:27 +1300

    Small refactor

--- test/codeVerifierShould.js
+++ test/codeVerifierShould.js
@@ -1,13 +1,14 @@
 var assert = require('assert');
-var codeVerifier = require('../codeVerifier');
+var codeVerifierFile = require('../codeVerifier');
+var isGuessCorrect = codeVerifierFile.isGuessCorrect;
 
 suite('CodeVerifier', function() {
     suite('#CodeVerifierShould', function() {
         test('return true for correct guess', function() {
-            assert.equal(codeVerifier.isGuessCorrect(['r', 'g', 'y', 'c'], ['r', 'g', 'y', 'c']), true);
+            assert.equal(isGuessCorrect(['r', 'g', 'y', 'c'], ['r', 'g', 'y', 'c']), true);
         });
         test('return false for incorrect test', function() {
-            assert.deepEqual(codeVerifier.isGuessCorrect(['r', 'g', 'y', 'c'], ['w', 'g', 'w', 'c']), false);
+            assert.deepEqual(isGuessCorrect(['r', 'g', 'y', 'c'], ['w', 'g', 'w', 'c']), false);
         });
     });
 });
