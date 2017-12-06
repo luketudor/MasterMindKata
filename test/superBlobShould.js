39e5d5c3613c1d4393d5befaaf2b1372c66562b7
luketudor <Luke.Tudor@myob.com>
2017-12-07 10:39:03 +1300

    Move code test into mark test, rename mark test to super blob

--- test/superBlobShould.js
+++ test/superBlobShould.js
@@ -5,6 +5,10 @@ function Mark(numBlackPegs, numWhitePegs) {
     this.numWhite = numWhitePegs;
 }
 
+function Code(orderedValues) {
+    this.values = orderedValues;
+}
+
 suite('Mark', function() {
     var mark;
     suite('#MarkPropertiesShould', function() {
@@ -23,6 +27,20 @@ suite('Mark', function() {
         test('return 2 white pegs', function() {
             mark = new Mark(1, 2);
             assert.equal(mark.numWhite, 2);
+        });
+    });
+});
+
+suite('Code', function() {
+    var code;
+    suite('#CodePropertiesShould', function() {
+        test('return correct code', function() {
+            code = new Code(['r', 'g', 'y', 'c']);
+            assert.deepEqual(code.values, ['r', 'g', 'y', 'c']);
+        });
+        test('return a different correct code', function() {
+            code = new Code(['w', 'g', 'w', 'c']);
+            assert.deepEqual(code.values, ['w', 'g', 'w', 'c']);
         });
     });
 });
