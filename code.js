64556c9701c565c2a15fc323aa6fddd9d7f995b8
luketudor <Luke.Tudor@myob.com>
2017-12-08 09:06:52 +1300

    Refactor black mark counter to use closure

--- code.js
+++ code.js
@@ -5,21 +5,22 @@ var Code = function(orderedValues) {
 };
 
 Code.prototype.mark = function(guess) {
-    var blackCount = 0;
-    var whiteCount = 0;
     var codeNonBlacks = [];
     var guessNonBlacks = [];
-
-    for (var i = 0; i < this.values.length; i++) {
-        var colour = guess.values.shift();
-        if (this.values[i] === colour) {
-            blackCount++;
-        } else {
-            guessNonBlacks.push(colour);
-            codeNonBlacks.push(this.values[i]);
+    var blackCounter = function(codeValues) {
+        for (var i = 0; i < codeValues.length; i++) {
+            var colour = guess.values.shift();
+            if (codeValues[i] !== colour) {
+                guessNonBlacks.push(colour);
+                codeNonBlacks.push(codeValues[i]);
+            }
         }
-    }
+        return codeValues.length - codeNonBlacks.length;
+    };
 
+    var blackCount = blackCounter(this.values);
+
+    var whiteCount = 0;
     for (var i = 0; i < guessNonBlacks.length; i++) {
         var matchIndex = codeNonBlacks.indexOf(guessNonBlacks[i]);
         if (matchIndex !== -1) {
